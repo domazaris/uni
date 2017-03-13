2202e596048b1f4f6f07b5284bfb6f49bb26d645
Dominic Azaris <dominic.azaris@nyriad.com>
2017-03-13 23:21:14 +1300

    compress "works"

--- COMP-203/StringSet/StringSet.java
+++ COMP-203/StringSet/StringSet.java
@@ -10,35 +10,69 @@ class StringSet
     public void add( String str )
     {
         // Check to see if the string exists
-        if( !hasString( str ) )
+        if( hasString( str ) )
+        {
             return;
+        }
         
         // Add the string to the array
         strings[ count++ ] = str;
         
         // Check the size of the array
         if( count == MAX )
+        {
             growArray();
+        }
     }
 
     public boolean hasString( String str )
     {
+        // Get the index of the string
+        int index = indexOf( str );
+
+        // If there is no instance of the string, the index will be -1
+        if( index == -1 )
+        {
+            return false;
+        }
         return true;
     }
 
     public int indexOf( String str )
     {
-        return 1;
+        // Loop through and compare each element with str
+        for( int i = 0; i < count; i++ )
+        {
+            if( strings[i].compareTo( str ) == 0 )
+            { 
+                // Return the index
+                return i;
+            }
+        }
+        
+        // Return an error code
+        return -1;
     }
 
     public String stringAt( int index )
     {
-        return "";
+        // Bounds check
+        if( index < count )
+        {
+            return strings[index];
+        }
+        return null;
     }
 
     private void growArray()
     {
+        // Create a new array 10% larger
+        MAX += MAX/10;
+        String [] new_strings = new String[ MAX ];
 
+        // Copy the old array into the new array
+        System.arraycopy( strings, 0, new_strings, 0, count );
+        strings = new_strings;
     }
 
     // Members
