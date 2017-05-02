6a1e4dc94eee24006b23b2caded6ab59040f6246
Dominic Azaris <dominic.azaris@nyriad.com>
2017-05-02 19:12:27 +1200

    stub stuff out

---
+++ 203/BST/OddWords.java
@@ -0,0 +1,56 @@
+public class OddWords
+{
+    private static void lexicon( String filename )
+    {
+        // Create list reference
+        // Open file
+
+        // Read line at a time
+        while
+        {
+            // Split into words
+
+
+            // If tree is null
+            for( words )
+            {
+                if(null)
+                {
+                    // Create Tree with first word
+                }
+
+                // Check tree for word
+                if( found )
+                {
+                    // Remove
+                    // Print ( word  +  "REMOVED")
+                }
+                else
+                {
+                    // Insert
+                    // Print ( word  + "ADDED")
+                }
+            }
+        }
+
+        // Print "LEXICON"
+
+        // Print remainder of list
+
+        // Close file
+
+    }
+
+    public static void main( String args[] )
+    {
+
+        // Arg parse
+        if( args.length != 1 )
+        {
+            // Error usage
+        }
+
+        // Run function
+        lexicon( args[ 1 ] );
+    }
+}
