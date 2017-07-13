642bb8359fb826a642e6090aeda0eab90611656f
domazaris <dominic.azaris@outlook.com>
2017-07-13 17:50:18 +1200

    fix formatting of cmd

--- 301/lab1/cmd.c
+++ 301/lab1/cmd.c
@@ -6,73 +6,81 @@
 #include <errno.h>
 #include <stdlib.h>
 
-void prompt() {
-  fprintf( stdout, "CMD $ " );
-  fflush( stdout );
+void prompt() 
+{
+    fprintf( stdout, "CMD $ " );
+    fflush( stdout );
 }
 
-int get_cmd( char* cmd, char* params[] ) {
-  // Read a line from stdin
-  size_t max_len = 1024;
-  char* input = (char*)malloc( max_len );
-  memset( input, 0, max_len );
-  
-  size_t len;
-  fflush(stdin);
-  int err = getline( &input, &len, stdin);
-  if( err <= 1 ) {
-    //printf("no line\n");
-    return 1;
-  }
+int get_cmd( char* cmd, char* params[] ) 
+{
+    // Read a line from stdin
+    size_t max_len = 1024;
+    char* input = (char*)malloc( max_len );
+    memset( input, 0, max_len );
 
-  // Get the cmd by splitting at each space
-  int i = 0;
-  char* word;
-  word = strtok( input, " \n" );
+    size_t len;
+    fflush(stdin);
+    int err = getline( &input, &len, stdin);
+    if( err <= 1 ) 
+    {
+        //printf("no line\n");
+        return 1;
+    }
 
-  // Copy first arg into cmd
-  memcpy( cmd, word, strlen( word ) );
+    // Get the cmd by splitting at each space
+    int i = 0;
+    char* word;
+    word = strtok( input, " \n" );
 
-  // Iterate the rest of the string for any parameters
-  while( word != NULL ) {
-    params[ i++ ] = word;
-    word = strtok( NULL, " \n");
-  }
+    // Copy first arg into cmd
+    memcpy( cmd, word, strlen( word ) );
 
+    // Iterate the rest of the string for any parameters
+    while( word != NULL ) 
+    {
+        params[ i++ ] = word;
+        word = strtok( NULL, " \n");
+    }
 
-  // Clean buffer
-  free( input );
-  return 0;
+    // Clean buffer
+    free( input );
+    return 0;
 }
 
 int main() {
-  // Iterate forever
-  while( 1 ) {
-    // Prompt user
-    prompt();
+    // Iterate forever
+    while( 1 ) 
+    {
+        // Prompt user
+        prompt();
 
-    // Get the command input
-    char* cmd = malloc( 256 );
-    char* params[ 256 ] = {};
+        // Get the command input
+        char* cmd = malloc( 256 );
+        char* params[ 256 ] = {};
 
-    if( get_cmd( cmd, params ) == 1 ) {
-      continue;
-    }
- 
-    // Create a child and execute the given command
-    if( fork() != 0 ) {
-      // Wait for child to finish
-      waitpid( -1, NULL, 0 );
-      free( cmd );
-    }
-    else {
-      // Replace the child process with the given command
-      execvp( cmd, params );
+        if( get_cmd( cmd, params ) == 1 ) 
+        {
+            // No cmd given
+            continue;
+        }
+    
+        // Create a child and execute the given command
+        if( fork() != 0 ) 
+        {
+            // Wait for child to finish
+            waitpid( -1, NULL, 0 );
+            free( cmd );
+        }
+        else 
+        {
+            // Replace the child process with the given command
+            execvp( cmd, params );
 
-      // If we get here exec has failed
-      fprintf( stderr, "Failed to execute: %s\n", cmd );
-      fprintf( stderr, "ERROR: %s\n", strerror( errno ) );
+            // If we get here exec has failed
+            fprintf( stderr, "Failed to execute: %s\n", cmd );
+            fprintf( stderr, "ERROR: %s\n", strerror( errno ) );
+        }
     }
-  }
-  return 0;
+    return 0;
 }
