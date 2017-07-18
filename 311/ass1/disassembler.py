aedcc6899ffe51d8315f88603641d0c9dd147f68
Dominic Azaris <dominic.azaris@nyriad.com>
2017-07-18 19:48:22 +1200

    gotta do a label parse

--- 311/ass1/disassembler.py
+++ 311/ass1/disassembler.py
@@ -24,18 +24,77 @@ INSTRUCTIONS = {
     "1010" : "beqz"
 }
 
-def convert(line):
-    ''' Converts machine code line to WRAMP '''
-    words = line.split()
 
-    # Find function
-    key = words[0]
-    instruct = INSTRUCTIONS[key]
-    if not isinstance(instruct, str):
-        key = words[3]
-        print(instruct[key])
-    else:
-        print(instruct)
+def scan_labels(ilines)
+
+def convert(ilines, labels):
+    ''' Converts machine code line to WRAMP '''
+    LABELS = {}
+    LABEL_COUNT = 0
+    for line in ilines:
+        if line and (not line.isspace()):
+            words = line.split()
+
+            # Find instruction
+            output_line = []
+            key = words[0]
+            instruct = INSTRUCTIONS[key]
+            if not isinstance(instruct, str):
+                key = words[3]
+                output_line.append(instruct[key])
+            else:
+                output_line.append(instruct)
+
+            # Get sources/destination
+            if words[0] == "0000":
+                # Destination
+                output_line.append("$" + str(int(words[1], 2)) + ",")
+
+                # Sources
+                output_line.append("$" + str(int(words[2], 2)) + ",")
+                output_line.append("$" + str(int(words[7], 2)))
+
+            elif words[0] == "0001":
+                # Destination
+                output_line.append("$" + str(int(words[1], 2)) + ",")
+
+                # Source
+                output_line.append("$" + str(int(words[2], 2)) + ",")
+
+                # imm
+                output_line.append(hex(int("".join(words[4:]), 2)))
+
+            else:
+                # Destinations
+                if words[0] == "1000" and words[0] == "1001":
+                    output_line.append("$" + str(int(words[1], 2)) + ",")
+
+                # Sources
+                if words[0] != "0100":
+                    output_line.append("$" + str(int(words[2], 2)) + ",")
+
+                # Values/Labels
+                if words[0] == "1000" and words[0] == "1001":
+                    # Values
+                    output_line.append(hex(int("".join(words[4:]), 2)))
+                else:
+                    # Labels
+                    addr = "".join(words[3:])
+                    try:
+                        label = LABELS[addr]
+                        output_line.append(label)
+                    except KeyError:
+                        # New label
+                        label = "L" + str(LABEL_COUNT)
+                        output_line.append(label)
+
+                        # Add to map
+                        LABELS[addr] = label
+                        LABEL_COUNT += 1
+
+
+            output_line = " ".join(output_line)
+            print(output_line)
 
 def main():
     ''' main method '''
@@ -54,9 +113,8 @@ def main():
         print("ERROR File Not Found: " + args.input_file)
         exit(1)
 
-    for line in ilines:
-        if line and (not line.isspace()):
-            convert(line)
+    labels = scan_labels(ilines)
+    convert(ilines, labels)
 
 if __name__ == "__main__":
     main()
