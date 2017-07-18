f5af700420788496e948f7715dfec570bd62eff5
Dominic Azaris <dominic.azaris@nyriad.com>
2017-07-18 15:18:59 +1200

    finish assembler

--- 311/ass1/assembler.py
+++ 311/ass1/assembler.py
@@ -20,6 +20,9 @@ INSTRUCTIONS = {
                    "beqz" : ["1010", "0000", "ssss", "iiii", "iiii", "iiii", "iiii", "iiii"]
                }
 
+def space_out(string, length):
+    return ' '.join(string[i:i+length] for i in range(0,len(string),length))
+
 def main():
     # Parse args
     parser = argparse.ArgumentParser()
@@ -44,31 +47,104 @@ def main():
             # print(line)
             cleaned_lines.append(line)
     
+    # Parse labels and add to a map
+    PROGRAM_COUNTER = 0
+    LABELS = {}
+    for line in cleaned_lines:
+        # Fetched cmd, now increment pc
+        PROGRAM_COUNTER += 1
+        
+        # Split line to get the operand
+        operand = line.split()[0]
+
+        if ":" in operand:
+            # Found a label, add it to the map
+            label = str(operand.split(":")[0])
+            LABELS[label] = PROGRAM_COUNTER
+
     # Parse for instructions
+    PROGRAM_COUNTER = 0
     output = []
     for line in cleaned_lines:
-        keyword = line.split()[0]
-        try:
-            out = INSTRUCTIONS[keyword]
-            if out[0] == "0000":
-                out[1] = "xxxx"
-                out[2] = "xxxx"
-                out[7] = "xxxx"
-            elif out[0] == "0001" or out[0] == "1000" or out[0] == "1001":
-                out[1] = "zzzz"
-                out[2] = "zzzz"
-            elif out[0] == "0100":
-                pass
-            elif out[0] == "1011" or out[0] == "1010":
-                out[2] = "cccc"
+        # Fetched cmd, now increment pc
+        PROGRAM_COUNTER += 1
+        
+        # Split line to get the operand
+        operand = line.split()[0]
+
+        # Skip and labels
+        if ":" in operand:
+            continue
+
+        # Load machine code template
+        out = INSTRUCTIONS[operand]
+
+        # Parse to fill out template
+        if out[0] == "0000":
+            # Get the optargs
+            optargs = line.split()[1].split(",")
+
+            # Destination
+            out[1] = "{0:04b}".format(int(optargs[0][1:]))
+
+            # Sources
+            out[2] = "{0:04b}".format(int(optargs[1][1:]))
+            out[7] = "{0:04b}".format(int(optargs[2][1:]))
+
+        elif out[0] == "0001" or out[0] == "1000" or out[0] == "1001":
+            # Get the optargs
+            optargs = line.split()[1].split(",")
             
-            # Store the new output line
-            output.append(" ".join(out))
-        except KeyError:
-            output.append('err: ' + keyword)
-            # print("ERROR Unrecognized instruction: " + keyword)
-            # exit(1)
-            pass
+            # Destination
+            out[1] = "{0:04b}".format(int(optargs[0][1:]))
+            
+            # Source
+            out[2] = "{0:04b}".format(int(optargs[1][1:]))
+
+            # Get the address and convert to binary
+            preaddr = optargs[2]
+            addr = None
+            if "0b" in preaddr:
+                addr = "{0:016b}".format(int(preaddr[2:]))
+            elif "0x" in preaddr:
+                hex = int(preaddr[2:], 16)
+                addr = "{0:016b}".format(hex)
+            else:
+                addr = "{0:016b}".format(int(preaddr))
+
+            # add split up and add to the output
+            addr = space_out(addr, 4)
+            for val in range(4, 8):
+                out[val] = addr.split()[val - 4]
+
+        elif out[0] == "0100":
+            # Jump - convert the pc to binary at the given label
+            label = line.split()[1].split(",")[0]
+            addr = "{0:020b}".format(int(LABELS[label]))
+
+            # Split up into 4 bit segments
+            addr = space_out(addr, 4)
+            for val in range(3, 8):
+                out[val] = addr.split()[val - 3]
+
+        elif out[0] == "1011" or out[0] == "1010":
+            # Get optargs
+            optargs = line.split()[1].split(",")
+
+            # Put register in source field
+            out[2] = "{0:04b}".format(int(optargs[0][1:]))
+
+            # Get address
+            label = optargs[1]
+            addr ="{0:020b}".format(int(LABELS[label]))
+
+            # Space out address and add to output
+            addr = space_out(addr, 4)
+            for val in range(3, 8):
+                out[val] = addr.split()[val - 3]
+
+        # Store the new output line
+        output.append(" ".join(out))
 
     # Output into new file
     with open(args.input_file.split(".")[0] + ".output", "w") as ofile:
