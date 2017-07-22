3be09ff24663bdfa4e516da22ffc762cc9f7a3a5
Dominic Azaris <dominic.azaris@nyriad.com>
2017-07-22 19:44:12 +1200

    update test script

--- 311/ass1/test.sh
+++ 311/ass1/test.sh
@@ -4,6 +4,6 @@ echo "ASSEMBLER TEST"
 for TESTFILE in test1 test2 test3
 do
 	echo "Testing $TESTFILE"
-	./assembler.py -f testfiles/$TESTFILE.s
-	diff <(xxd $TESTFILE.bin) <(xxd testfiles/$TESTFILE.bin) -y
+	./assembler.py -i testfiles/$TESTFILE.s -o /tmp/out.bin
+	diff <(xxd /tmp/out.bin) <(xxd testfiles/$TESTFILE.bin)
 done
