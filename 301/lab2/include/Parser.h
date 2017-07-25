b11dd517e77aaf4a6d407e1331c2f6de17be0e2a
Dominic Azaris <dominic.azaris@nyriad.com>
2017-07-25 15:35:34 +1200

    fix up makefile, add parser placeholder

---
+++ 301/lab2/include/Parser.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stddef.h>
+#include "LinkedList.h"
+
+/**
+ * Parses a csv file and outputs a LinkedList with each line
+ * as a node.
+ * @param filename The input csv file.
+ * @returns A linked list of each line as a node.
+ */
+LinkedList parseFile( const char* filename )
+{
+  return NULL;
+}
---
+++ 301/lab2/src/Task1.c
@@ -0,0 +1,6 @@
+#include <stddef.h>
+
+int main( int argc, char* argv[] )
+{
+  return 0;
+}
