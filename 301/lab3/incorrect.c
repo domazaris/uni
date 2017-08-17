a5bf2d1f24552b75fc8fc733f82870f386c8f610
Dominic Azaris <dominic.azaris@nyriad.com>
2017-08-17 12:28:51 +1200

    incorrect finished

--- 301/lab3/incorrect.c
+++ 301/lab3/incorrect.c
@@ -17,18 +17,17 @@ void put_fork( fork_t* forks, size_t fork )
 
 void think()
 {
-    //fprintf(stderr,"t");
+    fprintf(stderr,"t");
 }
 
 void eat()
 {
-    //fprintf(stderr,"e");
+    fprintf(stderr,"e");
 }
 
 typedef struct arg
 {
     fork_t* forks;
-    pthread_barrier_t* barrier;
     size_t id;
 } arg_t;
 
@@ -36,24 +35,20 @@ void* run_philosopher( void* args )
 {
     fork_t* forks = ((arg_t*)args)->forks;
     size_t id = ((arg_t*)args)->id;
-    pthread_barrier_t barrier = *args->barrier;
-    size_t i = 0;
+
+    // Forever
     while( 1 )
     {
         think();
         take_fork( forks, id );
 
-        // Wait for threads to hit point
-        pthread_barrier_wait(&mybarrier);
         
         take_fork( forks, (id + 1) % N );
         eat();
         put_fork(forks, id );
         put_fork( forks, (id + 1) % N );
         
-        // Allows to not kill my cpu
-        if( i++ % 10000 == 0)
-            fprintf(stderr, ".");
+        // Perform a context switch to not eat the entire CPU
         usleep(0);
     }
     fprintf(stderr, ".");
@@ -63,9 +58,7 @@ void* run_philosopher( void* args )
 
 int main()
 {
-    pthread_t threads = malloc( sizeof(pthread_t) * N );
-    pthread_barrier_t barrier;
-    pthread_barrier_init(&barrier, NULL, N);
+    pthread_t* threads = malloc( sizeof(pthread_t) * N );
     fork_t* forks = malloc( sizeof(fork_t) * N );
 
     for( size_t i = 0; i < N; i++ )
@@ -73,7 +66,6 @@ int main()
         arg_t* args = malloc( sizeof(arg_t) );
         args->forks = forks;
         args->id = i;
-        args->barrier = &barrier;
         if( pthread_create( (pthread_t*)&threads[i], NULL, run_philosopher, (void*)args ) < 0 )
         {
             fprintf( stderr, "Pthread Error: %s\n", strerror( errno ) );
@@ -86,7 +78,7 @@ int main()
 
     for( size_t i = 0; i < N; i++)
     {
-        pthread_join( &threads[i], NULL);
+        pthread_join( threads[i], NULL);
     }
 
     return 0;
