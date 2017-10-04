96d2da68fb562fbca9138f01d739e5c845b53c06
Dominic Azaris <dominic.azaris@nyriad.com>
2017-10-05 11:26:00 +1300

    readme

---
+++ 311/ass4/README.md
@@ -0,0 +1,15 @@
+## COMP311-17B Assignment 4
+#### Dominic Azaris - 1297845 - djmha1
+
+---
+
+### Usage:
+#### Compile
+```
+make
+```
+
+#### Run comparison with original vs optimised
+```
+make test
+```
--- 311/ass4/test.sh
+++ 311/ass4/test.sh
@@ -9,3 +9,4 @@ time ./sobel resources/timberlea.pgm results/timberlea-edges.pgm
 
 printf "\n---- Comparison ----\n\n"
 md5sum results/timberlea-edges.pgm results/timberlea-edges_orig.pgm
+cmp results/timberlea-edges.pgm results/timberlea-edges_orig.pgm
