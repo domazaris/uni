5ff41748da996b20a580b6995501ece57b0f6094
Dominic Azaris <dominic.azaris@nyriad.com>
2017-10-05 11:23:28 +1300

    add test script for nice output

---
+++ 311/ass4/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p results
+
+printf "\n---- Original ----\n"
+time ./orig  resources/timberlea.pgm results/timberlea-edges_orig.pgm
+
+printf "\n---- Optimized ----\n"
+time ./sobel resources/timberlea.pgm results/timberlea-edges.pgm
+
+printf "\n---- Comparison ----\n\n"
+md5sum results/timberlea-edges.pgm results/timberlea-edges_orig.pgm
