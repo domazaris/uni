fa0a638e9e32d255f20e8eb0b26e748c9d8dfe62
Dominic Azaris <dominic.azaris@nyriad.com>
2017-10-09 09:58:33 +1300

    move tests to use /tmp

--- 311/ass4/test.sh
+++ 311/ass4/test.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-rm -rf results
-mkdir -p results
-rm -f /tmp/dom-orig_time.txt /tmp/dom-opt_time.txt
+rm -f /tmp/djmha1-orig_time.txt /tmp/djmha1-opt_time.txt /tmp/djmha1-timberlea.pgm
+cp resources/timberlea.pgm /tmp/djmha1-timberlea.pgm
 
 printf "\n---- Original ----\n"
-bash -c "time ./bin/original resources/timberlea.pgm results/timberlea-edges_orig.pgm" &> /tmp/dom-orig_time.txt
-cat /tmp/dom-orig_time.txt
+bash -c "time ./bin/original /tmp/djmha1-timberlea.pgm /tmp/djmha1-timberlea-edges_orig.pgm" &> /tmp/djmha1-orig_time.txt
+cat /tmp/djmha1-orig_time.txt
 
 printf "\n---- Optimized ----\n"
-bash -c "time ./bin/sobel resources/timberlea.pgm results/timberlea-edges.pgm" &>  /tmp/dom-opt_time.txt
-cat /tmp/dom-opt_time.txt
+bash -c "time ./bin/sobel /tmp/djmha1-timberlea.pgm /tmp/djmha1-timberlea-edges.pgm" &>  /tmp/djmha1-opt_time.txt
+cat /tmp/djmha1-opt_time.txt
 
 printf "\n---- Timing ----\n\n"
-
-ORIG_TIME=$(cat /tmp/dom-orig_time.txt | cut -d'm' -f2 | head -n2 | tail -n1 | cut -d's' -f1)
-OPT_TIME=$(cat /tmp/dom-opt_time.txt | cut -d'm' -f2 | head -n2 | tail -n1 | cut -d's' -f1)
+ORIG_TIME=$(cat /tmp/djmha1-orig_time.txt | cut -d'm' -f2 | head -n2 | tail -n1 | cut -d's' -f1)
+OPT_TIME=$(cat /tmp/djmha1-opt_time.txt | cut -d'm' -f2 | head -n2 | tail -n1 | cut -d's' -f1)
 SPEED_UP=$(echo "scale=2; $ORIG_TIME / $OPT_TIME" | bc -l)
 printf "Got $SPEED_UP times speed up\n"
 
 printf "\n---- Comparison ----\n\n"
-md5sum results/timberlea-edges.pgm results/timberlea-edges_orig.pgm
+md5sum /tmp/djmha1-timberlea-edges.pgm /tmp/djmha1-timberlea-edges_orig.pgm
 printf "\n"
-cmp    results/timberlea-edges.pgm results/timberlea-edges_orig.pgm
+cmp    /tmp/djmha1-timberlea-edges.pgm /tmp/djmha1-timberlea-edges_orig.pgm
