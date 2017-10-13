a696d9d06c380d1dbdb7d4e3c502941688dd308b
Dominic Azaris <dominic.azaris@nyriad.com>
2017-10-13 16:09:21 +1300

    add queue read func

--- 301/lab6/test.sh
+++ 301/lab6/test.sh
@@ -1,6 +1,25 @@
 #! /bin/bash
-
+# set -x
 echo "Hello World" > /tmp/file1.txt
 echo "Hello Mar" > /tmp/file2.txt
-/workspace/qcreate /tmp/queue1
-/workspace/qdelete /tmp/queue1
+./qcreate /myqueue
+./qread /myqueue
+
+./fwatcher /myqueue /tmp/file1.txt &
+./fwatcher /myqueue /tmp/file2.txt &
+
+touch /tmp/file1.txt
+echo "Hi UFO!" >> /tmp/file2.txt
+cat /tmp/file2.txt
+
+./qread /myqueue
+
+./qread /myqueue
+cp /tmp/file1.txt /tmp/file3.txt
+./qread /myqueue
+
+ps | grep fwatcher
+
+killall fwatcher
+
+./qdelete /myqueue
